330b23e938ba84c81ccbf172aa49e48b8fc5303f
Benjamin Schaaf <ben.schaaf@gmail.com>
2023-10-10 00:07:57 +1100

    Fix windows universal batch script

--- sbnf/sbnf-windows-universal.bat
+++ sbnf/sbnf-windows-universal.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    start "" "%~dp0sbnf-x86_64-pc-windows-msvc.exe" %*
+    "%~dp0sbnf-x86_64-pc-windows-msvc.exe" %*
 ) else (
-    start "" "%~dp0sbnf-i686-pc-windows-msvc.exe" %*
+    "%~dp0sbnf-i686-pc-windows-msvc.exe" %*
 )
 
 :end
